1b01799f98853818f1a4aa107df44344a9353199
Akash Roy <roy.akashcucse@gmail.com>
2025-08-10 12:53:34 +0530

    Update TestOccurence.c

--- DSA/C/Practice/TestOccurence.c
+++ DSA/C/Practice/TestOccurence.c
@@ -4,3 +4,11 @@ the maximum size of the array is size, and the number of elements
 present in the array is n. For example, if the array arr = 
 [1,1,1,2,2,3,4,4,4] the output should be like 1 occurs 3 times, 2 
 occurs 2 times, 3 occurs 1 time, and 4 occurs 3 times */
+#include<stdio.h>
+
+
+
+int main (){
+
+
+}
